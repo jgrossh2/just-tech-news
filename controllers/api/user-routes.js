742eb52198535ce77ccfe2e21948850c44d3379f
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-05 14:20:51 -0600

    if login/logout added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -106,7 +106,6 @@ router.post('/login', (req, res) => {
   });
 });
 router.post('/logout', (req, res) => {
-  console.log("test")
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
